0e3f7630d2c337980c732e12608b40bfe08c445d
Andrease11 <andreasegalottiabz@gmail.com>
2025-08-20 15:37:46 +0200

    things

--- lua/plugins/toggle.lua
+++ lua/plugins/toggle.lua
@@ -18,5 +18,28 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      require("toggleterm").setup(opts)
+
+      -- Create keymaps for terminals 1-9
+      for i = 1, 9 do
+        vim.keymap.set({ "n", "t" }, "<leader>t" .. i, function()
+          -- Close any open terminal first
+          local terminals = require("toggleterm.terminal").get_all()
+          for _, terminal in pairs(terminals) do
+            if terminal:is_open() then
+              terminal:close()
+            end
+          end
+
+          -- Open the requested terminal
+          local term = require("toggleterm.terminal").Terminal:new({
+            count = i,
+            direction = "float",
+          })
+          term:toggle()
+        end, { desc = "Toggle terminal " .. i })
+      end
+    end,
   },
 }
